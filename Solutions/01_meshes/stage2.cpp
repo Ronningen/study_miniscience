59cca2934aa769de75850c227901ab987f5ce45f
Ronningen <step-lap@mail.ru>
2023-02-06 14:15:18 +0300

    stage 2

--- Solutions/01_meshes/stage2.cpp
+++ Solutions/01_meshes/stage2.cpp
@@ -4,5 +4,27 @@
 
 int main(int argc, char **argv)
 {
-  
+    gmsh::initialize();
+
+    gmsh::model::add("stl");
+
+    gmsh::merge("test_geometry.stl");
+
+    gmsh::model::mesh::classifySurfaces(0.3, true, true, 0.6);
+    gmsh::model::mesh::createGeometry();
+
+    gmsh::vectorpair entities;
+    gmsh::model::getEntities(entities, 2);
+    std::vector<int> surfs;
+    for (auto surf : entities)
+        surfs.push_back(surf.second);
+    gmsh::model::geo::addVolume({gmsh::model::geo::addSurfaceLoop(surfs)});
+
+    gmsh::model::geo::synchronize();
+    gmsh::model::mesh::generate(3);
+
+    std::set<std::string> args(argv, argv + argc);
+    if (!args.count("-nopopup"))
+        gmsh::fltk::run();
+    gmsh::finalize();
 }
